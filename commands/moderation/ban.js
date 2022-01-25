b8dd1a88fce44bacd157215f52d0a5b16c916aac
Dark Reaper <reaper.san000@gmail.com>
2022-01-25 12:25:35 +0600

    added moderation commands

---
+++ commands/moderation/ban.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "ban",
+    category: "moderation",
+    permissions: ["BAN_MEMBERS"],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        const target = message.mentions.users.first();
+        if(target){
+            const memberTarget = message.guild.members.cache.get(target.id);
+            memberTarget.ban();
+            const Embed = new MessageEmbed().setTitle("WOH! when the ban").setDescription(`${target} has been banned by ${message.author.tag}`).setTimestamp()
+            message.channel.send({ embeds: [Embed] })
+        }else{
+            message.channel.send(`I couldn't ban ${target} huh...`);
+        }
+    }
+}
---
+++ commands/moderation/kick.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "kick",
+    category: "moderation",
+    permissions: ["KICK_MEMBERS"],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        const target = message.mentions.users.first();
+        if(target){
+            const memberTarget = message.guild.members.cache.get(target.id);
+            memberTarget.kick();
+            const Embed = new MessageEmbed().setTitle("BEWM!>< get outta here haha").setDescription(`${target} has been kicked by ${message.author.tag}`).setTimestamp()
+            message.channel.send({ embeds: [Embed] })
+        }else{
+            message.channel.send(`I couldn't kick ${target}, maybe you have skill issue :(( try again i guess`);
+        }
+    }
+}
