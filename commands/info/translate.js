021881b32c05f5eba6637f34723fb0a210f7159c
Dark Reaper <reaper.san000@gmail.com>
2022-01-30 09:45:59 +0600

    added translation command

---
+++ commands/info/translate.js
@@ -0,0 +1,21 @@
+const discord = require('discord.js')
+const translate = require('@iamtraction/google-translate')
+
+
+module.exports = {
+    name: "translate",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+
+        const txt = args.join(" ")
+        if(!txt) return message.channel.send("Invalid,please try again")
+        translate(txt, { to: 'en' }).then(res => {
+            message.channel.send(res.text)
+        }).catch(err => {
+            message.channel.send("Error occurred,try again")
+        })
+        
+    }
+}
