84547a6ee2e80ad0ece167d3ea79388c61052574
Dark Reaper <reaper.san000@gmail.com>
2022-02-03 22:32:07 +0600

    fixed status and presence

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,14 @@
 module.exports = {
   name: "ready",
   run: async (bot) => {
+    const {client} = bot
+    
     console.log("Logged in as " + bot.client.user.tag);
+
+    client.user.setPresence({
+      activities: [{name: `to p!help`,
+      type: `LISTENING`}],
+      status: `online`
+    });
   },
 };
