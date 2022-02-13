61bae4d3691c97faea409392c341c09b0c584d93
Dark Reaper <reaper.san000@gmail.com>
2022-02-13 19:07:57 +0600

    added minor changes

--- commands/fun/whois.js
+++ commands/fun/whois.js
@@ -14,10 +14,10 @@ module.exports = {
         .setAuthor({ name: `${target.username}`, iconURL: `${target.displayAvatarURL({ dynamic: true, size: 512 })}` })
         .setThumbnail(target.displayAvatarURL({dynamic: true}))
         .setColor("BLURPLE")
-        .addField("UserID", `${target.id}`, false)
-        .addField("Roles", `${member.roles.cache.map(r => r).join(' ')}`)
-        .addField("Server Member Since", `${moment(member.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(member.joinedAt).startOf('day').fromNow()}`)
-        .addField("Discord User Since", `${moment(target.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(target.createdAt).startOf('day').fromNow()}`)
+        .addField("UserID :", `${target.id}`, false)
+        .addField("Roles :", `${member.roles.cache.map(r => r).join(' ')}`)
+        .addField("Server Member Since :", `${moment(member.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(member.joinedAt).startOf('day').fromNow()}`)
+        .addField("Discord User Since :", `${moment(target.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(target.createdAt).startOf('day').fromNow()}`)
          message.reply({embeds: [response]});
     },
         
